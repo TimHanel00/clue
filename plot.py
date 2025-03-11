58fe3bffb53c2143c41d0317ce04749e739fb430
René Widera <r.widera@hzdr.de>
2025-03-11 15:48:48 +0100

    update plot tool

--- plot.py
+++ plot.py
@@ -1,47 +1,33 @@
 #!/usr/bin/env python
-import csv
+import os
 import sys
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
 
+def plot_particles(input_data):
+    df = pd.read_csv(input_data, dtype=np.float32)
+    df.columns = df.columns.str.strip()
+    max_clusterid = max(df["clusterId"])
 
-def read_csv_file(filename):
-    try:
-        with open(filename, "r") as file:
-            reader = csv.DictReader(file)
-            data = [row for row in reader]
-            return data
-    except FileNotFoundError:
-        print(f"File {filename} not found.")
-        return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+    plt.figure(dpi=200)
 
+    df_out = df[df.clusterId == -1] # Outliers
+    plt.scatter(df_out.x, df_out.y, s=10, marker='x', color='0.4')
+    for i in range(0, int(max_clusterid)+1):
+        dfi = df[df.clusterId == i] # ith cluster
+        plt.scatter(dfi.x, dfi.y, s=10, marker='.')
+    df_seed = df[df.isSeed == 1] # Only Seeds
+    plt.scatter(df_seed.x, df_seed.y, s=25, color='r', marker='*')
 
-def plot_particles(data):
-    x = [float(row[" x"]) for row in data]
-    y = [float(row[" y"]) for row in data]
-    cluster_ids = [int(row[" clusterId"]) for row in data]
+    plt.xlabel("x", fontsize=14)
+    plt.ylabel("y", fontsize=14)
 
-    # Create a custom color map
-    cmap = LinearSegmentedColormap.from_list("", ["blue", "purple", "red", "orange", "yellow"])
+    plt.grid()
 
-    plt.scatter(x, y, c=cluster_ids, cmap=cmap, s=1, alpha=0.8)
-    plt.colorbar(label="clusterId", fraction=0.046, pad=0.04)
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.title("Particle Plot")
-
-    # Set the background color to white
-    plt.gca().set_facecolor("white")
-
-    # Save the plot to an SVG file
     plt.savefig("result.svg", format="svg", dpi=1200)
 
-    # Show the plot
     plt.show()
-
 
 def main():
     if len(sys.argv) != 2:
@@ -49,10 +35,9 @@ def main():
         sys.exit(1)
 
     filename = sys.argv[1]
-    data = read_csv_file(filename)
 
-    if data:
-        plot_particles(data)
+    if os.path.isfile(filename):
+        plot_particles(filename)
 
 
 if __name__ == "__main__":
